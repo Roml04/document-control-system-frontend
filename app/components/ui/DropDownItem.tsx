35ce08ed79a00b18782767d2a8ebe718135e89d8
IT OJT <IT OJT@tsr.local>
2026-02-16 15:35:20 +0800

    Restrict access to approve and deny revisions

--- app/components/ui/DropDownItem.tsx
+++ app/components/ui/DropDownItem.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Icon } from "../primitives";
+import { useSessionStore } from "stores/sessionStore";
+import { isRoleAllowed } from "~/utils/isRoleAllowed";
 
 type DropDownItemProps = {
   title: string;
@@ -15,11 +17,12 @@ export default function DropDownItem({
   handleDeny,
 }: DropDownItemProps) {
   const [isShown, setIsShown] = useState(false);
+  const role = useSessionStore((state) => state.role);
 
   return (
     <li className="border border-slate-300 px-4 py-2 rounded-lg transition-all duration-200">
       <div
-        className="flex w-full px-4 py-1 cursor-pointer"
+        className="flex w-full py-1 cursor-pointer"
         onClick={() => setIsShown((prev) => !prev)}
       >
         <div className="flex w-full">
@@ -41,25 +44,27 @@ export default function DropDownItem({
           ${isShown ? "max-h-96 opacity-100 translate-y-0" : "max-h-0 opacity-0 -translate-y-1"}
         `}
       >
-        <div className="flex flex-col gap-2">
-          <div className="flex flex-col px-3 py-1 w-full border-l border-slate-300">
+        <div className="flex flex-col">
+          <div className="flex flex-col px-3 py-1 w-full border-l border-slate-300 mb-2">
             <h3 className="text-slate-400">Reason</h3>
             <p className="text-slate-400">{description}</p>
           </div>
-          <div className="flex w-full justify-end gap-2">
-            <button
-              onClick={handleApprove}
-              className="group px-4 py-2 rounded-lg hover:bg-black cursor-pointer"
-            >
-              <p className="text-black group-hover:text-white">Approve</p>
-            </button>
-            <button
-              onClick={handleDeny}
-              className="group px-4 py-2 rounded-lg cursor-pointer"
-            >
-              <p className="text-red-400 group-hover:text-red-600 ">Deny</p>
-            </button>
-          </div>
+          {isRoleAllowed(["coordinator", "superior", "admin"], role) && (
+            <div className="flex w-full justify-end gap-2">
+              <button
+                onClick={handleApprove}
+                className="group px-4 py-2 rounded-lg hover:bg-black cursor-pointer"
+              >
+                <p className="text-black group-hover:text-white">Approve</p>
+              </button>
+              <button
+                onClick={handleDeny}
+                className="group px-4 py-2 rounded-lg cursor-pointer"
+              >
+                <p className="text-red-400 group-hover:text-red-600 ">Deny</p>
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </li>
