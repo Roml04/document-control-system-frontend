e501e3751753894523df2617a479057e97db17e9
Rommel <garaisrommel04@gmail.com>
2026-09-06 16:34:26 +0800

    Create badge component for file type

---
+++ app/components/primitives/FileTypeBadge.tsx
@@ -0,0 +1,36 @@
+import { FileText, Form, ListTodo } from "lucide-react";
+import { FILETYPE } from "~/constants/enums";
+
+type FileTypeBadgePropType = {
+  type: FILETYPE;
+  size: number;
+};
+
+export default function FileTypeBadge({ type, size }: FileTypeBadgePropType) {
+  const className =
+    "flex justify-center items-center rounded-sm bg-gray-200 aspect-square h-14";
+  switch (type) {
+    case FILETYPE.DOCUMENT:
+      return (
+        <div className={className}>
+          <FileText size={size} />
+        </div>
+      );
+    case FILETYPE.CHECKLIST:
+      return (
+        <div className={className}>
+          <ListTodo size={size} />
+        </div>
+      );
+
+    case FILETYPE.FORM:
+      return (
+        <div className={className}>
+          <Form size={size} />
+        </div>
+      );
+
+    default:
+      return <div>Unknown Type</div>;
+  }
+}
