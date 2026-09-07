064da9ea03b0705a809bc166023549fdf9898f43
Rommel <garaisrommel04@gmail.com>
2026-09-07 23:37:22 +0800

    Make size attribute optional in file type badge component

--- app/components/primitives/FileTypeBadge.tsx
+++ app/components/primitives/FileTypeBadge.tsx
@@ -3,12 +3,11 @@ import { FILETYPE } from "~/constants/enums";
 
 type FileTypeBadgePropType = {
   type: FILETYPE;
-  size: number;
+  size?: number;
 };
 
 export default function FileTypeBadge({ type, size }: FileTypeBadgePropType) {
-  const className =
-    "flex justify-center items-center rounded-sm bg-gray-200 aspect-square h-14";
+  const className = `flex justify-center items-center rounded-sm bg-gray-200 aspect-square h-14`;
   switch (type) {
     case FILETYPE.DOCUMENT:
       return (
