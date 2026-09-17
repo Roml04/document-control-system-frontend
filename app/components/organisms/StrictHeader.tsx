7f578bb15beb285d37d28bc57b9fedfb795de809
Rommel <garaisrommel04@gmail.com>
2026-09-18 00:46:13 +0800

    Create strict header

---
+++ app/components/organisms/StrictHeader.tsx
@@ -0,0 +1,84 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog";
+import LoadingButton from "~/components/primitives/LoadingButton";
+import { Button } from "~/components/ui/button";
+import { ChevronLeft, RotateCcw } from "lucide-react";
+import { useNavigate } from "react-router";
+
+type StrictHeaderPropType = {
+  resetOnCLick: () => void;
+  isSpinning: boolean;
+};
+
+export default function StrictHeader({
+  resetOnCLick,
+  isSpinning,
+}: StrictHeaderPropType) {
+  const navigate = useNavigate();
+  return (
+    <header className="flex justify-between">
+      <AlertDialog>
+        <AlertDialogTrigger type="button" asChild>
+          <Button type="button" variant={"ghost"}>
+            <ChevronLeft />
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Leave this page?</AlertDialogTitle>
+            <AlertDialogDescription>
+              Your unsaved changes will be lost if you leave this page. Are you
+              sure you want to continue?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Stay on page</AlertDialogCancel>
+            <AlertDialogAction onClick={() => navigate(-1)}>
+              Leave page
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <div className="flex gap-1">
+        <AlertDialog>
+          <AlertDialogTrigger type="button" asChild>
+            <Button type="button" variant={"ghost"}>
+              <RotateCcw />
+              Reset
+            </Button>
+          </AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Reset your progress?</AlertDialogTitle>
+              <AlertDialogDescription>
+                Your current progress will be lost. Are you sure you want to
+                start over?
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Keep editing</AlertDialogCancel>
+              <AlertDialogAction onClick={resetOnCLick}>
+                Reset
+              </AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+        <LoadingButton
+          type="submit"
+          displayText="Resubmit"
+          loadingDisplayText="Resubmit"
+          isSpinning={isSpinning}
+        />
+      </div>
+    </header>
+  );
+}
